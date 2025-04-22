7e4fb8f83e730de66bdcc4bd5c01a2ac605e105f
Jonathan <jonalinux.uk@gmail.com>
2025-04-22 23:59:04 +0100

    Update epg.js

--- frontend/js/epg.js
+++ frontend/js/epg.js
@@ -32,7 +32,7 @@ async function loadEPG(channelName) {
 
   let epgMap = {};
   try {
-    const res = await fetch('/backend/epg/epg-sources.json');
+    const res = await fetch('https://raw.githubusercontent.com/JonathanSanfilippo/RaspServerTV/refs/heads/main/backend/epg/epg-sources.json');
     if (!res.ok) throw new Error('Errore nel fetch epg-sources');
     epgMap = await res.json();
   } catch (err) {
