56c4e74a22b7acdaf8d2c26652f01e4c6f31373d
Jonathan <jonalinux.uk@gmail.com>
2025-04-22 20:33:50 +0100

    Delete frontend/js/m3u.js

--- frontend/js/m3u.js
+++
@@ -1,6 +0,0 @@
-export const m3uUrls = [
-  'https://raw.githubusercontent.com/JonathanSanfilippo/iptv-auto-cleaner/refs/heads/main/lists/original/original.m3u',
-  ''
-];
-
-export const defaultRaspberryPiLogo = '../frontend/img/logo.png';
